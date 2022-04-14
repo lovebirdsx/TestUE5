a0bd2c289ae8e4b4c2a99b9ee5b5486bdcffc97f
lovebird <lovebirdsx@gmail.com>
2022-04-14 16:54:26 +0800

    up

--- Content/JavaScript/Editor/Common/Log.js
+++ Content/JavaScript/Editor/Common/Log.js
@@ -3,16 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.error = exports.warn = exports.log = void 0;
 /* eslint-disable spellcheck/spell-checker */
 const ue_1 = require("ue");
+function getTime() {
+    const date = new Date();
+    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
 function log(msg) {
-    ue_1.MyLog.Log(msg);
+    ue_1.MyLog.Log(`${getTime()}: ${msg}`);
 }
 exports.log = log;
 function warn(msg) {
-    ue_1.MyLog.Warn(msg);
+    ue_1.MyLog.Warn(`${getTime()}: ${msg}`);
 }
 exports.warn = warn;
 function error(msg) {
-    ue_1.MyLog.Error(msg);
+    ue_1.MyLog.Error(`${getTime()}: ${msg}`);
 }
 exports.error = error;
 //# sourceMappingURL=Log.js.map
--- TypeScript/Editor/Common/Log.ts
+++ TypeScript/Editor/Common/Log.ts
@@ -1,14 +1,19 @@
 /* eslint-disable spellcheck/spell-checker */
 import { MyLog } from 'ue';
 
+function getTime(): string {
+    const date = new Date();
+    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
 export function log(msg: string): void {
-    MyLog.Log(msg);
+    MyLog.Log(`${getTime()}: ${msg}`);
 }
 
 export function warn(msg: string): void {
-    MyLog.Warn(msg);
+    MyLog.Warn(`${getTime()}: ${msg}`);
 }
 
 export function error(msg: string): void {
-    MyLog.Error(msg);
+    MyLog.Error(`${getTime()}: ${msg}`);
 }

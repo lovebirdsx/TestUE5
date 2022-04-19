f5a411abb5c779dc0cdda5ccacdaaf9ed2c710b0
lovebird <lovebirdsx@gmail.com>
2022-04-19 20:45:48 +0800

    up

--- Tools/watch.sh
+++ Tools/watch.sh
@@ -1,6 +1,5 @@
 cd ..
-# rm Content/JavaScript/Game -rf
-# rm Content/JavaScript/Editor -rf
-# rm Content/JavaScript/Common -rf
-# tsc-watch --onSuccess "node ./Tools/send_editor_command.js 0 RestartEditor"
+rm Content/JavaScript/Game -rf
+rm Content/JavaScript/Editor -rf
+rm Content/JavaScript/Common -rf
 tsc-watch --onSuccess "sh Tools/on_watch_success.sh"

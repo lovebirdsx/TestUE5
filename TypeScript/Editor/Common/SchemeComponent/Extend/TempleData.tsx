0be87c28f0b3cdb6f5f6073a9ae3ab6de119cbfc
WhiteCrow <460333649@qq.com>
2022-06-14 18:21:30 +0800

    single模板选择添加entityType筛选

--- TypeScript/Editor/Common/SchemeComponent/Extend/TempleData.tsx
+++ TypeScript/Editor/Common/SchemeComponent/Extend/TempleData.tsx
@@ -5,15 +5,18 @@ import { HorizontalBox, VerticalBox } from 'react-umg';
 
 import {
     getComponentsTypeByEntityType,
+    getEntityTypeByActor,
     getEntityTypeByBlueprintType,
 } from '../../../../Game/Interface/Entity';
 import { IEntityTemplate, TComponentsData } from '../../../../Game/Interface/IEntity';
 import { ComponentsData } from '../../../EntityEditor/ComponentsData';
 import { Btn, Fold, TAB_OFFSET, Text } from '../../BaseComponent/CommonComponent';
 import { FilterableList } from '../../BaseComponent/FilterableList';
+import { entityListCache } from '../../EntityListCache';
 import { entityTemplateManager } from '../../EntityTemplateManager';
 import { IProps, TModifyType } from '../../Type';
 import { openFile } from '../../Util';
+import { entityIdContext } from '../Context';
 
 interface IState {
     IsFolded: boolean;
@@ -84,28 +87,38 @@ export class TempleData extends React.Component<IProps<number>, IState> {
         );
     }
 
+    private RenderSelect(entityId): JSX.Element {
+        const entity = entityListCache.GetEntityById(entityId);
+        const type = getEntityTypeByActor(entity);
+        return (
+            <HorizontalBox>
+                <Fold
+                    IsFold={this.state.IsFolded}
+                    OnChanged={this.OnFoldChange}
+                    IsFull={true}
+                    Tip={'展开模板信息'}
+                />
+                <Text Text={'模板：'} />
+                <FilterableList
+                    Tip={'实体模板名字'}
+                    Items={entityTemplateManager.GetNamesByEntityType(type)}
+                    Selected={entityTemplateManager.GetNameById(this.props.Value)}
+                    OnSelectChanged={(name: string): void => {
+                        this.props.OnModify(entityTemplateManager.GetIdByName(name), 'normal');
+                    }}
+                />
+                <Btn Text={'◉'} OnClick={this.OpenEntityTemplate} Tip={'打开实体模板'} />
+            </HorizontalBox>
+        );
+    }
+
     // eslint-disable-next-line @typescript-eslint/naming-convention
     public render(): JSX.Element {
         return (
             <VerticalBox>
-                <HorizontalBox>
-                    <Fold
-                        IsFold={this.state.IsFolded}
-                        OnChanged={this.OnFoldChange}
-                        IsFull={true}
-                        Tip={'展开模板信息'}
-                    />
-                    <Text Text={'模板：'} />
-                    <FilterableList
-                        Tip={'实体模板名字'}
-                        Items={entityTemplateManager.Names}
-                        Selected={entityTemplateManager.GetNameById(this.props.Value)}
-                        OnSelectChanged={(name: string): void => {
-                            this.props.OnModify(entityTemplateManager.GetIdByName(name), 'normal');
-                        }}
-                    />
-                    <Btn Text={'◉'} OnClick={this.OpenEntityTemplate} Tip={'打开实体模板'} />
-                </HorizontalBox>
+                <entityIdContext.Consumer>
+                    {(entityId: number): JSX.Element => this.RenderSelect(entityId)}
+                </entityIdContext.Consumer>
                 <VerticalBox RenderTransform={{ Translation: { X: TAB_OFFSET } }}>
                     {!this.state.IsFolded && this.RenderTemple()}
                 </VerticalBox>
